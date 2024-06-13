75fe5dfc546741342e43d068ccc44611f7522193
Talha Berkay Akbulut <49911006+NovaDiablox@users.noreply.github.com>
2024-06-13 21:07:25 +0300

    Add files via upload

---
+++ lang_tr.lua
@@ -0,0 +1,31 @@
+return {
+	["state.waiting"] = "OYUNCULAR BEKLENİYOR",
+	["state.headstart"] = "ÜSTÜNLÜK",
+	["state.ongoing"] = "KALAN SÜRE",
+	["state.postround"] = "RAUNT BİTTİ",
+
+	["winteam.drivers"] = "SÜRÜCÜLER KAZANDI",
+	["winteam.seekers"] = "EBELER KAZANDI",
+	["winteam.none"] = "RAUNT BİTTİ",
+
+	["winreason.time"] = "SÜRE DOLDU",
+	["winreason.seekersleft"] = "EBELER MAÇI TERK ETTİ",
+	["winreason.driverscaught"] = "TÜM SÜRÜCÜLER YAKALANDI",
+	["winreason.driversleft"] = "SÜRÜCÜLER MAÇI TERK ETTİ",
+	["winreason.none"] = "Raunt yönetim gücüyle yeniden başlatıldı",
+
+	["hud.headstartrelease"] = "ŞU SÜRE İÇİNDE SERBEST BIRAKILACAKSIN",
+	["hud.unstuck.hold"] = "Sıkışmaktan kurtulmak için %s tuşunu basılı tutun",
+	["hud.unstuck.press"] = "Sıkışmaktan kurtulmak için %s tuşuna basın",
+	["hud.respawn"] = "Yeniden doğmak için %s tuşuna basın",
+	["hud.ping"] = "En yakın rakip oyuncuyu belirtmek için %s tuşuna basın",
+	["hud.kmh"] = "km/sa",
+
+	["alert.lastdriver"] = "SON KALAN SÜRÜCÜ",
+
+	["scoreboard.respawn"] = "Yeniden Doğ",
+	["scoreboard.respawn.unavailable"] = "Yalnızca raunt sırasında Ebeler tarafından kullanılabilir",
+
+	["console.langnotfound"] = "'%s' dili için dil dosyası bulunamadı. Geçerli Dil '%s'.",
+	["console.langloaded"] = "'%s' dili başarıyla yüklendi.",
+}
